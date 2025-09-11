605fa14eb33aad5f6385cbd8a5d8697cdd068741
joisephdev <joisephdev@gmail.com>
2025-09-11 11:18:44 -0400

    Update BodyConfig to default wrapInContainer to true for improved layout handling

--- lib/src/components/scaffold/scaffold_params.dart
+++ lib/src/components/scaffold/scaffold_params.dart
@@ -55,7 +55,7 @@ class BodyConfig {
   final String? exitMessage;
 
   const BodyConfig({
-    this.wrapInContainer = false,
+    this.wrapInContainer = true,
     this.decoration,
     this.resizeToAvoidBottomInset = false,
     this.exitMessage,
