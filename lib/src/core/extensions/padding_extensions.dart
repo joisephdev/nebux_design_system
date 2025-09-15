7745935a4c828ccf08b7428ff5fd2c2cfb0c194d
joisephdev <joisephdev@gmail.com>
2025-09-14 21:47:42 -0400

    Rename padding extension methods for consistency and clarity in padding_extensions.dart

--- lib/src/core/extensions/padding_extensions.dart
+++ lib/src/core/extensions/padding_extensions.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 extension PaddingExtensions on Widget {
   /// Add padding to all sides of the widget
-  /* Widget paddingAll(double padding) {
+  /* Widget nbxPaddingAll(double padding) {
     return Padding(padding: EdgeInsets.all(padding), child: this);
   } */
 
   /// Add padding to the left side of the widget
-  Widget paddingLeft(double padding) {
+  Widget nbxPaddingLeft(double padding) {
     return Padding(
       padding: EdgeInsets.only(left: padding),
       child: this,
@@ -15,7 +15,7 @@ extension PaddingExtensions on Widget {
   }
 
   /// Add padding to the right side of the widget
-  Widget paddingRight(double padding) {
+  Widget nbxPaddingRight(double padding) {
     return Padding(
       padding: EdgeInsets.only(right: padding),
       child: this,
@@ -23,7 +23,7 @@ extension PaddingExtensions on Widget {
   }
 
   /// Add padding to the top side of the widget
-  Widget paddingTop(double padding) {
+  Widget nbxPaddingTop(double padding) {
     return Padding(
       padding: EdgeInsets.only(top: padding),
       child: this,
@@ -31,7 +31,7 @@ extension PaddingExtensions on Widget {
   }
 
   /// Add padding to the bottom side of the widget
-  Widget paddingBottom(double padding) {
+  Widget nbxPaddingBottom(double padding) {
     return Padding(
       padding: EdgeInsets.only(bottom: padding),
       child: this,
@@ -39,7 +39,7 @@ extension PaddingExtensions on Widget {
   }
 
   /// Add padding to the left and right sides of the widget
-  Widget paddingSymmetric({double horizontal = 0.0, double vertical = 0.0}) {
+  Widget nbxPaddingSymmetric({double horizontal = 0.0, double vertical = 0.0}) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: horizontal, vertical: vertical),
       child: this,
@@ -47,7 +47,7 @@ extension PaddingExtensions on Widget {
   }
 
   /// Add padding to the left and right sides of the widget
-  Widget paddingHorizontal(double padding) {
+  Widget nbxPaddingHorizontal(double padding) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: padding),
       child: this,
@@ -55,7 +55,7 @@ extension PaddingExtensions on Widget {
   }
 
   /// Add padding to the top and bottom sides of the widget
-  Widget paddingVertical(double padding) {
+  Widget nbxPaddingVertical(double padding) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: padding),
       child: this,
@@ -63,7 +63,7 @@ extension PaddingExtensions on Widget {
   }
 
   /// Add padding to the top, left, right, and bottom sides of the widget
-  Widget paddingOnly({
+  Widget nbxPaddingOnly({
     double left = 0.0,
     double top = 0.0,
     double right = 0.0,
