b4fb4220e8d243e8c05aff8bdba400acbe6292d1
Eddy Ruben <eddy@wedotransformations.com>
2026-02-06 08:38:11 -0500

    updating phone input widget

--- lib/src/components/inputs/text_fields/nbx_phone_field_widget.dart
+++ lib/src/components/inputs/text_fields/nbx_phone_field_widget.dart
@@ -122,22 +122,21 @@ class _NbxPhoneFieldWidgetState extends State<NbxPhoneFieldWidget> {
           maxLength: _currentCountrySelected?.maxLength,
           isReadOnly: _isPhoneInputReadOnly,
           controller: _numberPhoneController,
-          customValidator: (value) {
-            final result = NbxInputValidator.validateWithRules(value, [
-              TextValidationRules.minLength(
-                minLength: _currentCountrySelected?.minLength ?? 0,
-              ),
-              TextValidationRules.maxLength(
-                maxLength: _currentCountrySelected?.maxLength ?? 0,
-              ),
-            ]);
 
-            Future.delayed(const Duration(milliseconds: 100), () {
-              widget.phoneNumberInputParameters.customValidator?.call(result);
-            });
+          customValidator:
+              widget.phoneNumberInputParameters.customValidator ??
+              (value) {
+                final result = NbxInputValidator.validateWithRules(value, [
+                  TextValidationRules.minLength(
+                    minLength: _currentCountrySelected?.minLength ?? 0,
+                  ),
+                  TextValidationRules.maxLength(
+                    maxLength: _currentCountrySelected?.maxLength ?? 0,
+                  ),
+                ]);
 
-            return result;
-          },
+                return result;
+              },
         ),
       ),
     );
