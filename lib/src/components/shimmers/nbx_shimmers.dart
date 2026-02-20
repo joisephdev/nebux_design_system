4974819e3ef50deb97f4a1c5f50aa8bc96b83970
joisephdev <joisephdev@gmail.com>
2026-02-19 22:43:48 -0500

    refactor: rename `ShimmerContainerStyle` to `ShimmerSkeletonStyle` in shimmers components and documentation.

--- lib/src/components/shimmers/nbx_shimmers.dart
+++ lib/src/components/shimmers/nbx_shimmers.dart
@@ -18,7 +18,7 @@
 ///
 /// // Basic shimmer container
 /// NbxShimmerContainer(
-///   style: ShimmerContainerStyle(
+///   style: ShimmerSkeletonStyle(
 ///     width: 200,
 ///     height: 100,
 ///   ),
@@ -26,8 +26,8 @@
 ///
 /// // Shimmer list tile
 /// NbxShimmerListTile(
-///   titleStyle: ShimmerContainerStyle(width: 150, height: 20),
-///   subtitleStyle: ShimmerContainerStyle(width: 100, height: 16),
+///   titleStyle: ShimmerSkeletonStyle(width: 150, height: 20),
+///   subtitleStyle: ShimmerSkeletonStyle(width: 100, height: 16),
 /// )
 /// ```
 library;
